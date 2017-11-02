2d6042a609a28faacae569d82e7678e1ad0f6b5f
최춘기 <ckchoi@ysmcorp.com>
2017-11-02 18:44:46 +0900

    Java8 Lambda :: Chapter6

---
+++ java8-lambda/src/main/java/chapter6/sector02/SetViews.java
@@ -0,0 +1,21 @@
+package chapter6.sector02;
+
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class SetViews {
+
+    public static void main(String[] args) {
+
+        Set<String> words = ConcurrentHashMap.<String>newKeySet();
+
+        ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
+        words = map.keySet(1L);
+        words.add("Java");
+        System.out.println(map.get("Java"));
+
+    }
+
+}
